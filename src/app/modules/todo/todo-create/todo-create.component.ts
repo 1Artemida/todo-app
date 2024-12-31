055118b3e5d8409012c39a6da32c24620c3c51ed
1Artemida <darkhella2102@gmail.com>
2024-12-31 17:51:06 +0100

    initial structure

---
+++ src/app/modules/todo/todo-create/todo-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-create.component.html',
+  styleUrl: './todo-create.component.css'
+})
+export class TodoCreateComponent {
+
+}
